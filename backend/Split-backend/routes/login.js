004b276dec0398c4036c6dd2c61bd9d1588e8888
unknown <guanweilong@pjlab.org>
2024-06-26 14:14:47 +0800

    Store the user login data into the database

---
+++ backend/Split-backend/routes/login.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+var express = require('express');
+var router = express.Router();
+
+/* GET login page. */
+router.post('/', function(req, res, next) {
+  // Connect to MongoDB.
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  async function run() {
+    try {
+      await client.connect();
+      const database = client.db('test');
+      const collection = database.collection('users');
+      const query = { user_id: req.body.user_id };
+      const user = await collection.findOne(query);
+      if (user) {
+        // Redirect to the user profile page.
+      } else {
+        const query = { user_id: req.body.user_id, display_name: req.body.display_name, email: req.body.email };
+        const user = await collection.insertOne(query);
+        // Redirect to the user profile page.
+      }
+    } finally {
+      await client.close();
+    }
+  }
+});
